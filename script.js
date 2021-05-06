dc2468fb1a9f352e10b13099219b7085076d43ec
Unknown-CloudDev <officialosadeba@gmail.com>
2021-05-06 23:52:40 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal*years*rate/100;
+    var year = new Date().getFullYear()+parseInt(years); 
+
+      document.getElementById("result").innerHTML = " if you deposit <mark>1000000</mark> at an interest of <mark>3.5%</mark>. You will receive an amount of <mark>175000</mark>, in the year <mark>2025</mark>"
+
     
+    //p = document.getElementById("principal").value;
+    
+
 }
-        
+    function updateRate()
+    {
+        var rateval = document.getElementById("rate").value;
+        document.getElementById("rate_val").innerText=rateval;
+    }    
+
+   
