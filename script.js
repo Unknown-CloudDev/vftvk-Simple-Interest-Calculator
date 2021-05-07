dbe9edbb19de5a4bce853905c940693cad7add93
Unknown-CloudDev <officialosadeba@gmail.com>
2021-05-07 00:29:38 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -3,16 +3,20 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal*years*rate/100;
+    document.getElementById("result").innerHTML = principal*years*rate/100;
     var year = new Date().getFullYear()+parseInt(years); 
 
-      document.getElementById("result").innerHTML = " if you deposit <mark>1000000</mark> at an interest of <mark>3.5%</mark>. You will receive an amount of <mark>175000</mark>, in the year <mark>2025</mark>"
-
-    
-    //p = document.getElementById("principal").value;
-    
-
+      //document.getElementById("result").innerHTML = " if you deposit <mark>1000000</mark> at an interest of <mark>3.5%</mark>. You will receive an amount of <mark>175000</mark>, in the year <mark>2025</mark>"
 }
+
+//function calculate()
+{
+      //  num1 = document.getElementById("firstNumber").value;
+      //  num2 = document.getElementById("secondNumber").value;
+      //  num3 = document.getElementById("thirdNumber").value;
+     //   document.getElementById("result").innerHTML = num1 * num2 * num3/100;
+}
+
     function updateRate()
     {
         var rateval = document.getElementById("rate").value;
